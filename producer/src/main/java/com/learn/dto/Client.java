da21452218efe513f926611e83a0edd4d217414d
Dzmitry Ramaniuk <dzmitry.ramaniuk@gmail.com>
2024-06-17 16:07:54 +0200

    Add integration tests for producers

--- producer/src/main/java/com/learn/dto/Client.java
+++ producer/src/main/java/com/learn/dto/Client.java
@@ -1,8 +1,14 @@
 package com.learn.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Client {
     private Long clientId;
     private String email;
--- producer/src/main/java/com/learn/dto/Transaction.java
+++ producer/src/main/java/com/learn/dto/Transaction.java
@@ -1,10 +1,16 @@
 package com.learn.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Transaction {
     private String bank;
     private Long clientId;
